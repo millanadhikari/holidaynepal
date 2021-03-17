2d1ae314540f17689284c8634f21f25e858a1203
Xreethapa <shreethapa8@gmail.com>
2021-03-17 18:43:22 +1100

    Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Test from './components/test';
 
 function App() {
   return (
     <div className="App">
      <p>holiday nepal in nepal</p>
+    <Test />
     </div>
   );
 }
