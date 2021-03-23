7e049fbfc1a82e57814a6a07113f45c52c0e58b5
millanadhikari <millanadhikari@gmail.com>
2021-03-23 18:40:56 +1100

    New fresh mobile

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import './App.css';
 
 
-import Destinations from './components/Destinations';
-import Footer from './components/Footer';
-import Joinhost from './components/Joinhost';
-import Test from './components/test';
-import Home from './Home'
-import Nearbyplaces from './components/Nearbyplaces'
-
 function App() {
   return (
     <div className="App">
 
 
      
-    
-
-            {/* <Header /> */}
- <Home/>
-      <Nearbyplaces/>
-    <Joinhost/>
-    
-      <Destinations/>
-      <Footer/>
-      
-
+    <p>Holiday Nepal</p>
     </div>
   );
 }
